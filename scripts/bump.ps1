3859bc7cb36e3c44cb343824f073da0ae0aa37e3
Ayaka <KiritaniAyaka@users.noreply.github.com>
2024-10-13 14:57:33 +0800

    chore: clearer bump message

--- scripts/bump.ps1
+++ scripts/bump.ps1
@@ -2,7 +2,7 @@ $newVersion = Invoke-Expression "git cliff --bumped-version"
 $versionWithoutV = $newVersion.Replace("v", "")
 Write-Output "Bumping to version $versionWithoutV"
 
-Invoke-Expression "pnpm exec bumpp $versionWithoutV -qyp false --commit 'chore(release): release v%s'"
+Invoke-Expression "pnpm exec bumpp $versionWithoutV -qyp false --commit 'chore(release): v%s'"
 
 Invoke-Expression "git cliff -o CHANGELOG.md"
 Write-Output "Updated CHANGELOG.md"
--- scripts/bump.sh
+++ scripts/bump.sh
@@ -3,7 +3,7 @@ newVersion=$(git cliff --bumped-version)
 versionWithoutV=${newVersion#v}
 echo "Bumping to version $versionWithoutV"
 
-pnpm exec bumpp $versionWithoutV -qyp false --commit "chore(release): release v%s"
+pnpm exec bumpp $versionWithoutV -qyp false --commit "chore(release): v%s"
 
 git cliff -o CHANGELOG.md
 echo "Updated CHANGELOG.md"
